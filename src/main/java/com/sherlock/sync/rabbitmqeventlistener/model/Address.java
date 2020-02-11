f6d9e940897c5237cc6ec4bc09e8153ae0cec4ae
Sandeep <sandeep.lakdawala@upgrad.com>
2020-02-11 11:08:33 +0530

    Adding Lombok

--- src/main/java/com/sherlock/sync/rabbitmqeventlistener/model/Address.java
+++ src/main/java/com/sherlock/sync/rabbitmqeventlistener/model/Address.java
@@ -1,5 +1,14 @@
 package com.sherlock.sync.rabbitmqeventlistener.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
 public class Address {
 
 	private String addressType;
@@ -8,63 +17,4 @@ public class Address {
 	private String city;
 	private String state;
 	private String country ;
-
-	public String getAddressType() {
-		return addressType;
-	}
-
-	public void setAddressType(String addressType) {
-		this.addressType = addressType;
-	}
-
-	public String getLine1() {
-		return line1;
-	}
-
-	public void setLine1(String line1) {
-		this.line1 = line1;
-	}
-
-	public String getLien2() {
-		return lien2;
-	}
-
-	public void setLien2(String lien2) {
-		this.lien2 = lien2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	@Override public String toString() {
-		return "Address{" +
-				"addressType='" + addressType + '\'' +
-				", line1='" + line1 + '\'' +
-				", lien2='" + lien2 + '\'' +
-				", city='" + city + '\'' +
-				", state='" + state + '\'' +
-				", country='" + country + '\'' +
-				'}';
-	}
 }
--- src/main/java/com/sherlock/sync/rabbitmqeventlistener/model/Event.java
+++ src/main/java/com/sherlock/sync/rabbitmqeventlistener/model/Event.java
@@ -1,42 +1,18 @@
 package com.sherlock.sync.rabbitmqeventlistener.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
 public class Event {
-
 	private String orderId;
 	private List<Product> products;
 	private Recipient recipient;
-
-	public String getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(String orderId) {
-		this.orderId = orderId;
-	}
-
-	public List<Product> getProducts() {
-		return products;
-	}
-
-	public void setProducts(List<Product> products) {
-		this.products = products;
-	}
-
-	public Recipient getRecipient() {
-		return recipient;
-	}
-
-	public void setRecipient(Recipient recipient) {
-		this.recipient = recipient;
-	}
-
-	@Override public String toString() {
-		return "Event{" +
-				"orderId='" + orderId + '\'' +
-				", products=" + products +
-				", recipient=" + recipient +
-				'}';
-	}
 }
